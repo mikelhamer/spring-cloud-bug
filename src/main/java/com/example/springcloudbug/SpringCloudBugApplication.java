b915b56c954aa929c29267a700c4cc34041e680f
mikelhamer <mikelhamer@gmail.com>
2022-12-17 00:39:22 -0500

    Initial commit

---
+++ src/main/java/com/example/springcloudbug/SpringCloudBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudbug/SpringCloudBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
